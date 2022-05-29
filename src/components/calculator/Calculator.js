acee597f3099222a339fd24ea02c43e0a1366da9
Chris Mitchell <chrism0318@gmail.com>
2022-05-29 16:53:30 -0700

    Fixed capitalization issue

---
+++ src/components/calculator/Calculator.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Calculator';
+import ButtonPanel from '../ButtonPanel';
+import Display from "../Display";
+
+
+export default class Calculator extends React.Component {
+
+  render() {
+    return (
+      <div className="component-calculator">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
+};
